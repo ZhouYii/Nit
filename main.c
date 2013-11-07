14ec37a97d1c1b8959d411029125469da06e3b3a
yigh <yizhou12345@gmail.com>
2013-11-07 03:26:18 -0600

    Modular

--- main.c
+++ main.c
@@ -4,12 +4,8 @@
 #include <sys/types.h>
 #include <unistd.h>
 #include <glib.h>
-
-#define BUF_SIZE 1024
-#define REPO_OFFSET 2
-#define SHA512_SIZE 128
-#define NO_CMD "Not a valid command. Try Nit help.\n"
-
+#include "helpers.c"
+#include "const.c"
 
 
 /* Populate current directory with required files for starting a repo. 
@@ -17,16 +13,10 @@
 char init_repo();
 char add_file(const char* path);
 char push();
-const char* hashfile(const char* filepath);
-char copy_file(char* src_path, char* dest_path);
 char commit();
 void serialize();
 void revert(const int revision);
-void exec(const char* cmd);
-void err_handler(const char* msg);
 void set_up_nit();
-void strip_newline(char *buff);
-void skiplines(FILE* f, int lines);
 void delete(char* path);
 
 int revision = -1;
@@ -89,6 +79,7 @@ void delete(char* path) {
     del = path;
 }
 
+/*  Sets up structures for a first-run of nit */
 void set_up_nit() {
     FILE* db = fopen(".nitdb", "w");
     char cwd[BUF_SIZE];
@@ -97,72 +88,6 @@ void set_up_nit() {
     fprintf(db, "0\n%s\n", cwd);
     exec("mkdir .nit");
     exec("mkdir .nit/0");
-}
-
-/* Calculates SHA512 has for a file specified by filepath.
- * mallocs so calling function must remember to free
- * The buffer returned is of size SHA512_SIZE and null terminated*/
-const char* hashfile(const char* filepath) {
-    size_t new_len;
-    if(filepath == NULL)
-        return FALSE;
-    FILE* f = fopen(filepath, "r");
-    if(f == NULL)
-        return FALSE;
-
-    GChecksum *chksum = g_checksum_new(G_CHECKSUM_SHA512);
-    char buf[BUF_SIZE + 1] = {0};
-    do {
-        new_len = fread((void*) buf, sizeof(char), BUF_SIZE, f);
-        g_checksum_update(chksum, (void*) buf, new_len);
-    } while(new_len != 0);
-
-    char* hash_str = malloc(sizeof(char) * (SHA512_SIZE+1));
-    hash_str[SHA512_SIZE] = '\0';
-    strcpy(hash_str, g_checksum_get_string(chksum));
-    g_checksum_free(chksum);
-    return hash_str;
-}
-
-char copy_file(char* src_path, char* dest_path) {
-    if(src_path == NULL || dest_path == NULL)
-        return FALSE;
-
-    FILE *src = fopen(src_path, "r");
-    if(src == NULL) {
-        printf("Failed to open src file\n");
-        return FALSE;
-    }
-    FILE *dest = fopen(dest_path, "w");
-    if(dest == NULL) {
-        printf("Failed to create destination file\n");
-        return FALSE;
-    }
-
-    char read;
-    while((read = getc(src)) != EOF) 
-        putc(read, dest);
-
-    return TRUE;
-
-}
-
-void err_handler(const char* msg) {
-    if(msg)
-        printf("%s\n", msg);
-    exit(1);
-}
-
-/* When reading serialized data back from filesystem, the newly inserted
- * newline characters corrupt data. This function removed the last newline char
- * from strings */
-void strip_newline(char *buf) {
-    if(buf == NULL)
-        return;
-    int i = 0;
-    while(buf[i] != '\0')
-        i++;
-    buf[--i] = '\0';
 }
 
 /* Check to see if the revision and hashtable are initialized. If not,
@@ -205,49 +130,6 @@ char init_repo() {
     return TRUE;
 }
 
-char db_findline(const char* line) {
-    char *buf = malloc(sizeof(char) * (BUF_SIZE + 1));
-    if(line == NULL || buf == NULL)
-        return FALSE;
-    int len; 
-    size_t buf_size = BUF_SIZE;
-    buf[BUF_SIZE] = '\0';
-    FILE *db = fopen(".nitdb", "r");
-    if(db == NULL)
-        err_handler("Failed to open file in db_findline");
-    while((len = getline(&buf, &buf_size, db)) != -1) {
-        strip_newline(buf);
-        if(strcmp(line, buf) == 0) {
-            free(buf);
-            return TRUE;
-        }
-    }
-    free(buf);
-    return FALSE;
-}
-
-char db_writeline(const char* line) {
-    FILE *db = fopen(".nitdb", "a");
-    if(db == NULL)
-        err_handler("Failed to open file in db_writeline");
-    if(fprintf(db, "%s", line) < 0)
-        err_handler("Failed to write to database");
-    fflush(db);
-    return TRUE;
-}
-
-void exec(const char *cmd) {
-    if(cmd == NULL)
-        return;
-    int len = strlen(cmd);
-    char * buffer = malloc(sizeof(char) * (len + 18));
-    if(buffer == NULL) 
-        return;
-    strncpy(buffer, cmd, len);
-    strcpy((buffer + len), " > /dev/null 2>&1");
-    system(buffer);
-    free(buffer);
-}
 
 /*  Reverts to an older version.
  *  TODO : more error checking.
@@ -317,36 +199,9 @@ char push() {
     return FALSE;
 }
 
-int num_digits(int n) {
-    int i = 0;
-    do {
-        i++;
-        n /= 10;
-    } while(n != 0);
-    return i;
-}
 
-char* itoc(int val, int digits) {
-    char *buf = malloc(sizeof(char) * (digits + 1));
-    if(buf == NULL) 
-        return NULL;
-    buf[digits] = '\0';
-    for(int i = (digits-1); i >= 0; i--) {
-        buf[i] = '0' + (val % 10);
-        val /= 10;
-    }
-    return buf;
-}
-
-void skiplines(FILE* f, int lines) {
-    if(f == NULL)
-        return;
-    char c;
-    while(lines > 0 && (c = fgetc(f)) != EOF) 
-        if(c == '\n')
-            lines--;
-}
-
+/*  Commit changes to local filesystem.
+ */
 void serialize() {
     if(revision < 0)
         return; 
@@ -370,8 +225,6 @@ void serialize() {
     skiplines(b, REPO_OFFSET);
     while(getline(&buf, &buf_size, b) != -1) {
         strip_newline(buf);
-        if(del)
-            printf("BUF: %s DEL: %s STRCMP: %d\n", buf, del, strcmp(del, buf));
         if(del && (strcmp(del, buf) == 0)) {
             skiplines(b, 1);
         } else
